b6d486fc631c3df48df2eb263a818c8af69dde3f
tzhd427 <115906803+xiaoniao427@users.noreply.github.com>
2026-02-27 09:47:33 +0800

    Create ads.js
    把广告相关逻辑分离出来

---
+++ ads.js
@@ -0,0 +1,15 @@
+// ads.js
+export const AD_CODE = `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4002076249242835"
+     crossorigin="anonymous"></script>
+<!-- 页内正方形广告 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4002076249242835"
+     data-ad-slot="7425310120"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+`;
