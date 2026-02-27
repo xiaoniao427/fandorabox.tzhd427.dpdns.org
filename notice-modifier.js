199ca733660dc9ddd96492a113cead6ec93a1604
tzhd427 <115906803+xiaoniao427@users.noreply.github.com>
2026-02-27 10:36:55 +0800

    Update notice-modifier.js
    修改公告

--- notice-modifier.js
+++ notice-modifier.js
@@ -1,6 +1,6 @@
 // 自定义公告内容
 const CUSTOM_NOTICE_CONTENT = `
-（此为代理站，原站地址：fandorabox.net）
+（此为代理站，原站地址：https://fandorabox.net ）
 （本站目前仍处于早期试验阶段）FANdoraBOX，或者说fanNET，也就是给你看这条公告的网站，是由FANdoraxxx建立的一个谱面下载站和游玩分数上传/存储站（与MajdataPlay工作）。原站完全不盈利，服务器和网络等成本均由原站作者一人承担且不接受捐赠，也因此无法保证网站上任何内容的可靠性，持久性，安全性（但是咱会尽力的……）。
 如果你对本站的使用有任何大大或小小疑惑，可以通过以下任意方式联系原站作者（如果是关于连接至majplay的疑问，建议先看教程/加入QQ群）：
 QQ账号：187299431 Gmail：takamichika666@gmail.com QQ Mail：187299431@qq.com
